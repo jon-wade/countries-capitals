1bcfce205893a20ce959d0fa8158b3371f24dce5
jon-wade <jonwadeuk@gmail.com>
2016-04-23 11:58:26 +0100

    add factory method to retrieve country list JSON file

--- app/myApp.js
+++ app/myApp.js
@@ -1,4 +1,25 @@
 angular.module('myApp', ['ngRoute', 'ngAnimate'])
+    .factory('getCountries', ['$http', function($http){
+        var url='http://api.geonames.org/countryInfoJSON';
+        var params = {
+            'username': 'jonwade'
+        };
+
+        return $http({
+            'url': url,
+            'params': params,
+            'method': 'GET'
+        })
+            .success(function(response){
+                console.log('SUCCESS!');
+                console.log(response);
+            })
+            .error(function(response){
+                console.log('ERROR!');
+                console.log(response);
+            });
+
+    }])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider
             .when('/', {
@@ -21,8 +42,15 @@ angular.module('myApp', ['ngRoute', 'ngAnimate'])
         }
 
     }])
-    .controller('countriesCtrl', [function(){
+    .controller('countriesCtrl', ['$location', '$scope', 'getCountries', function($location, $scope, getCountries){
         //countries page controller code here
+        $scope.redirect = function(){
+            $location.path('/');
+        };
+
+        getCountries;
+
+
     }])
     .controller('capitalCtrl', [function(){
         //capital page controller code here
